f6301eee7a951539786b92a3fca7b3dccda91bd7
tomashku <tzuczek@mac.com>
2022-06-26 18:58:28 +0300

    Initial Commit

---
+++ rain_finder_1.4/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  rain_finder_1.4
+//
+//  Created by Tomasz Zuczek on 26/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rain_finder_1.4/rain_finder_1_4App.swift
@@ -0,0 +1,17 @@
+//
+//  rain_finder_1_4App.swift
+//  rain_finder_1.4
+//
+//  Created by Tomasz Zuczek on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct rain_finder_1_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
